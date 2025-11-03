f635bb21fa193d0130c8dad9b381cb14c5a3d970
snoozie-v <gettingsleepyz@gmail.com>
2025-11-02 20:19:55 -0600

    change style on game list

--- src/Components/Utilities/GameList.jsx
+++ src/Components/Utilities/GameList.jsx
@@ -3,7 +3,7 @@ const GameList = ({
   onGameClick,
   getStatusColor,
   getStatusContent,
-  gridLayout,
+  gridLayout, // Can keep this prop for compatibility, but it's unused now
   getTeamInfo,
 }) => {
   const groupedGames = games.reduce((acc, game) => {
@@ -33,138 +33,70 @@ const GameList = ({
   return sortedDates.map((date) => (
     <div key={date}>
       <h3 className="text-lg font-medium mt-4 mb-2">{date}</h3>
-      {gridLayout ? (
-        <div className="grid grid-cols-2 gap-4">
-          {groupedGames[date].map((game, index) =>
-            (() => {
-              const competition = game.competitions[0];
-              const away = competition.competitors.find(
-                (c) => c.homeAway === "away"
-              );
-              const home = competition.competitors.find(
-                (c) => c.homeAway === "home"
-              );
-              const { name: awayName, logo: awayLogo } = getTeamInfo(away);
-              const { name: homeName, logo: homeLogo } = getTeamInfo(home);
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {groupedGames[date].map((game) => {
+          const competition = game.competitions[0];
+          const away = competition.competitors.find(
+            (c) => c.homeAway === "away"
+          );
+          const home = competition.competitors.find(
+            (c) => c.homeAway === "home"
+          );
+          const { name: awayName, logo: awayLogo } = getTeamInfo(away);
+          const { name: homeName, logo: homeLogo } = getTeamInfo(home);
 
-              return (
-                <div
-                  key={game.id}
-                  className={`border-b pb-2 hover:bg-gray-200 cursor-pointer rounded-md ${getStatusColor(
-                    game
-                  )} h-32 flex items-center justify-center`}
-                  onClick={() => onGameClick(game.id)}
-                >
-                  <div className="flex flex-col items-center text-center p-2 pt-4 text-gray-800">
-                    <div className="flex items-center mb-2">
-                      <img
-                        src={awayLogo}
-                        alt={`${awayName} logo`}
-                        className="w-10 h-10 mr-2 rounded-full object-contain"
-                        onError={(e) => {
-                          e.target.style.display = "none";
-                          e.target.nextSibling.style.display = "inline";
-                        }}
-                      />
-                      <span
-                        className="text-xl font-bold"
-                        style={{ display: "none" }}
-                      >
-                        ⚾
-                      </span>
-                      <span className="mx-1 text-lg">@</span>
-                      <img
-                        src={homeLogo}
-                        alt={`${homeName} logo`}
-                        className="w-10 h-10 ml-2 rounded-full object-contain"
-                        onError={(e) => {
-                          e.target.style.display = "none";
-                          e.target.nextSibling.style.display = "inline";
-                        }}
-                      />
-                      <span
-                        className="text-xl font-bold"
-                        style={{ display: "none" }}
-                      >
-                        ⚾
-                      </span>
-                    </div>
-                    <div className="text-lg font-semibold">
-                      {away.team.shortDisplayName} @{" "}
-                      {home.team.shortDisplayName}
-                    </div>
-                    <div className="text-xl mt-2">{getStatusContent(game)}</div>
-                  </div>
+          return (
+            <div
+              key={game.id}
+              className={`border-b pb-2 hover:bg-gray-200 cursor-pointer rounded-md ${getStatusColor(
+                game
+              )} h-32 flex items-center justify-center`}
+              onClick={() => onGameClick(game.id)}
+            >
+              <div className="flex flex-col items-center text-center p-2 pt-4 text-gray-800">
+                <div className="flex items-center mb-2">
+                  <img
+                    src={awayLogo}
+                    alt={`${awayName} logo`}
+                    className="w-10 h-10 mr-2 rounded-full object-contain"
+                    onError={(e) => {
+                      e.target.style.display = "none";
+                      e.target.nextSibling.style.display = "inline";
+                    }}
+                  />
+                  <span
+                    className="text-xl font-bold"
+                    style={{ display: "none" }}
+                  >
+                    ⚾
+                  </span>
+                  <span className="mx-1 text-lg">@</span>
+                  <img
+                    src={homeLogo}
+                    alt={`${homeName} logo`}
+                    className="w-10 h-10 ml-2 rounded-full object-contain"
+                    onError={(e) => {
+                      e.target.style.display = "none";
+                      e.target.nextSibling.style.display = "inline";
+                    }}
+                  />
+                  <span
+                    className="text-xl font-bold"
+                    style={{ display: "none" }}
+                  >
+                    ⚾
+                  </span>
                 </div>
-              );
-            })()
-          )}
-        </div>
-      ) : (
-        <ul className="space-y-4">
-          {groupedGames[date].map((game) => {
-            const competition = game.competitions[0];
-            const away = competition.competitors.find(
-              (c) => c.homeAway === "away"
-            );
-            const home = competition.competitors.find(
-              (c) => c.homeAway === "home"
-            );
-            const { name: awayName, logo: awayLogo } = getTeamInfo(away);
-            const { name: homeName, logo: homeLogo } = getTeamInfo(home);
-
-            return (
-              <li
-                key={game.id}
-                className={`border-b pb-2 hover:bg-gray-200 cursor-pointer rounded-md ${getStatusColor(
-                  game
-                )}`}
-                onClick={() => onGameClick(game.id)}
-              >
-                <div className="flex justify-between items-center p-2 text-gray-800">
-                  <div className="flex items-center">
-                    <img
-                      src={awayLogo}
-                      alt={`${awayName} logo`}
-                      className="w-8 h-8 mr-2 rounded-full object-contain"
-                      onError={(e) => {
-                        e.target.style.display = "none";
-                        e.target.nextSibling.style.display = "inline";
-                      }}
-                    />
-                    <span
-                      className="text-lg font-bold"
-                      style={{ display: "none" }}
-                    >
-                      ⚾
-                    </span>
-                    <span className="mx-2">@</span>
-                    <img
-                      src={homeLogo}
-                      alt={`${homeName} logo`}
-                      className="w-8 h-8 ml-2 rounded-full object-contain"
-                      onError={(e) => {
-                        e.target.style.display = "none";
-                        e.target.nextSibling.style.display = "inline";
-                      }}
-                    />
-                    <span
-                      className="text-lg font-bold"
-                      style={{ display: "none" }}
-                    >
-                      ⚾
-                    </span>
-                    <span className="text-lg font-bold ml-2">
-                      {awayName} @ {homeName}
-                    </span>
-                  </div>
-                  <span className="text-md">{getStatusContent(game)}</span>
+                <div className="text-lg font-semibold">
+                  {away.team.shortDisplayName} @{" "}
+                  {home.team.shortDisplayName}
                 </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+                <div className="text-xl mt-2">{getStatusContent(game)}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   ));
 };
